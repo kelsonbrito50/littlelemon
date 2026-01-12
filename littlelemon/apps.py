602e4f2e7ac1d2317a045d0353a38070f26b9bcd
Kelson Brito <kelsonbrito50@gmail.com>
2026-01-12 07:36:18 -0600

    Update README with detailed instructions

--- littlelemon/apps.py
+++ littlelemon/apps.py
@@ -1,9 +1,6 @@
 from django.apps import AppConfig
 
+
 class LittlelemonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-<<<<<<< HEAD
     name = 'littlelemon'
-=======
-    name = 'littlelemon'
->>>>>>> e8ddac1 (Add screenshots)
